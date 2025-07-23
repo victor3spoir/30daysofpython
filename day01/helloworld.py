2677959f322f5598399b0b0c14c7870699af0cef
kvruntime <kvruntime@gmail.com>
2025-07-23 14:14:09 +0000

    working on first day exercices

---
+++ day01/helloworld.py
@@ -0,0 +1,42 @@
+## Exercice 2
+# question 1
+from math import sqrt
+import os
+
+
+# Exercice 2
+print("Exercice 2")
+## question 1
+print("question-1")
+print(os.system("python --version"))
+
+
+## question 2
+print("question-2")
+print(10 + 3)  # addition
+print(10 - 3)  # soustraction
+print(10 / 3)  # division
+print(10 * 3)  # multiplication
+print(10 % 3)  # modulo
+print(10 ^ 3)  # exposant
+print(10 // 3)  # division entière
+
+## question - 3
+print("question-3")
+print("Victor" + " Espoir" + "Togo" + "je profite de 30 jours de python")
+
+## question - 4
+print(type(10 // 9.8 // 4))
+print(type(["Asabeneh", "Python", "Finlande"]))
+print(type("Victor" + " Espoir" + "Togo" + "je profite de 30 jours de python"))
+
+# Exercice 3
+print("Exercice 3")
+## question 1
+print(" question-1")
+## question 2
+print(" question-2")
+point1 = (2, 3)
+point2 = (10, 8)
+distance_euclidienne = sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+print(" distance euclidienne: ", distance_euclidienne)
