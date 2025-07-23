6fea139544b13f6d9d1506b505b3321e94eb98ea
kvruntime <kvruntime@gmail.com>
2025-07-23 18:24:16 +0000

    update code

--- day01/helloworld.py
+++ day01/helloworld.py
@@ -1,5 +1,3 @@
-## Exercice 2
-# question 1
 from math import sqrt
 import os
 
@@ -8,7 +6,7 @@ import os
 print("Exercice 2")
 ## question 1
 print("question-1")
-print(os.system("python --version"))
+os.system("python --version")
 
 
 ## question 2
@@ -26,8 +24,8 @@ print("question-3")
 print("Victor" + " Espoir" + "Togo" + "je profite de 30 jours de python")
 
 ## question - 4
-print(type(10 // 9.8))
-print(type(3.14))
+print(type(10 // 9))
+print(type(8 // 3.14))
 print(type(4 - 4j))
 print(type(["Asabeneh", "Python", "Finlande"]))
 print(type("Victor" + " Espoir" + "Togo" + "je profite de 30 jours de python"))
@@ -35,10 +33,16 @@ print(type("Victor" + " Espoir" + "Togo" + "je profite de 30 jours de python"))
 # Exercice 3
 print("Exercice 3")
 ## question 1
+print("String data")
+print(1 == 1, 2 == 3)
+print([1, "list", 2, "amorphe"])
+print((10, 8))
+print({1, 2, 3, 7, 10})
+print({"username": "runner", "password": ".......", "age": 20, "is_married": False})
 print(" question-1")
 ## question 2
 print(" question-2")
 point1 = (2, 3)
 point2 = (10, 8)
-distance_euclidienne = sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+distance_euclidienne = sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
 print(" distance euclidienne: ", distance_euclidienne)
