9716ae39fef1c28d412e37de1b095a9c99fe8c3a
kvruntime <kvruntime@gmail.com>
2025-07-24 14:04:19 +0000

    work on day2

---
+++ day02/variables.py
@@ -0,0 +1,79 @@
+# Jour 2: 30 Days of Python Programming
+# exercice level 1
+first_name = "name"
+last_name = "nom de famille"
+full_name = "nom complet"
+country = "un pays quelconque"
+city = "une valeur aussi"
+age = 99
+year = 2025
+is_married = True
+is_true = False
+is_light_on = False
+mutliple = True
+variable = "for_sure"
+last_one = "last"
+
+# exercice level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(mutliple))
+print(type(variable))
+print(type(last_one))
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+# 4
+num_one = 5
+print(num_one)
+num_two = 4
+print(num_two)
+# 5
+total = num_one + num_two
+print(total)
+# 6
+diff = num_two - num_one
+print(diff)
+# 7
+product = num_two * num_one
+print(product)
+# 8
+division = num_one / num_two
+print(division)
+# 9
+remainder = num_two % num_one
+print(remainder)
+# 10
+exp = num_one**num_two
+print(exp)
+# 11
+floor_division = num_one // num_two
+print(floor_division)
+# 12
+radius = 30
+pi = 22 / 7
+## 1 area
+area_of_circle = radius**2 * pi
+print(area_of_circle)
+## 2 circum
+circum_of_circle = radius * 1 * pi
+print(circum_of_circle)
+## 3 user input
+user_radius_input = int(input("Enter the radius value(only number are supported!): "))
+new_area = user_radius_input**2 * pi
+print(new_area)
+# 13
+first_name = input("Enter your first_name: ")
+last_name = input("Enter your last_name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+# 14
+help(reversed)
