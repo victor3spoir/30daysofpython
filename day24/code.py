092a7bdb12a84672466d4e6d7e3bde697a3a06cc
victor3spoir <victorespoir.dev@gmail.com>
2025-08-15 20:37:22 +0000

    complete day-24

---
+++ day24/code.py
@@ -0,0 +1,9 @@
+###day-24
+# exercice
+# to add package requirements in env. `uv add numpy matplotlib`
+# this consist to repeat all example in the pdf files
+# the exercice instruct to repeat all examples
+# i didn't guess if example must be copied & paste here in file or not
+# i there is even jupyternotebook provided with all examples
+# i tried all of them
+# i can also tell that i use numpy & pandas daily in work for computing simulations (in solar photovoltaic systems field)
